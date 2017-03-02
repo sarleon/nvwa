4ff66020647566edf8dd3ac26a5da0c3ff3c0704
sarleon <sarleon56@hotmail.com>
2017-03-02 18:05:33 +0800

    first basic edtion

--- remove_space.js
+++ remove_space.js
@@ -6,9 +6,18 @@
 
 
 */
+console.log('test_begin');
 function remove_space_in_adjancent_character_and_dot(string) {
 
-    var regx1=/([\u4E00-\u9FA5\uF900-\uFA2D])\s+([\uFF00-\uFFEF])/;
-    string=string.replace(regx1,$1+$2);
+    var regx_after=/([\u4E00-\u9FA5\uF900-\uFA2D])\s+([\uFF00-\uFFEF\,\.])/g;
+    string=string.replace(regx_after,"$1$2");
+
+    var regx_before=/([\uFF00-\uFFEF\,\.])\s+([\u4E00-\u9FA5\uF900-\uFA2D])/g;
+    string=string.replace(regx_before,"$1$2");
     return string;
 }
+
+
+
+
+document.body.innerHTML=remove_space_in_adjancent_character_and_dot(document.body.innerHTML);
